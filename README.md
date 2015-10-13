eee397b428df762da4f835d1056df638b2c7f4ad
Alberto Mijares <alberto.mijares@edorasware.com>
2015-10-13 17:23:54 +0200

    (fix): remove file output and add documentation

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # webpack-bundle-report
+
 Reporting tool to analyse Webpack bundles
+
+## Installation:
+
+```
+npm i -g webpack-bundle-report
+```
+
+## Usage from JS:
+
+```javascript
+var webpackBundleReport = require('webpack-bundle-report'),
+    webpackConfigurationPath = path.resolve('webpack.config.js'),
+    webpackConfiguration = require(webpackConfigurationPath);
+
+webpackBundleReport(webpackConfiguration, function (error, report) {
+    if (error) {
+        throw new Error(error);
+    }
+
+    console.log(report);
+});
+```
+
+## Usage from CLI:
+
+```
+wrb webpack.config.js > report.txt
+```
--- cli.js
+++ cli.js
@@ -1,24 +1,14 @@
 #!/usr/bin/env node
 
 var path = require('path'),
-    fs = require('fs'),
     webpackBundleReport = require('./'),
     webpackConfigurationPath = path.resolve(process.argv[2] || 'webpack.config.js'),
-    webpackConfiguration = require(webpackConfigurationPath),
-    outputFileName = process.argv[3];
+    webpackConfiguration = require(webpackConfigurationPath);
 
 webpackBundleReport(webpackConfiguration, function (error, report) {
     if (error) {
         throw new Error(error);
     }
 
-    if (outputFileName) {
-        fs.writeFile(outputFileName, report, function (err) {
-            if (err) {
-                throw new Error(err);
-            }
-        });
-    } else {
-        console.log(report);
-    }
+    console.log(report);
 });
